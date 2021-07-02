83b5d23bb273612034c78e44429bdd7388b3b6cd
Silas Daniel <danielsilas23@yahoo.com>
2021-07-02 11:22:23 +0300

    semantic ui setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import HomePage from './components/HomePage'
+import LoginPage from './components/LoginPage'
 const App = () => (
-  <div>
+  <div className="ui container">
     <Route path="/" exact component = { HomePage }/>
     <Route path="/login" exact component = { LoginPage }/>
   </div>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+import 'semantic-ui-css/semantic.min.css'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter} from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
 <BrowserRouter>
