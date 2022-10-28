0e2c05317686f3425fe443f0ae961f6b2da501b9
silasdani <danielsilas23@yahoo.com>
2022-10-29 01:46:02 +0300

    fix depl

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
 const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
-
-
 const port = process.env.PORT || 8080;
 const app = express();
 app.use(favicon(__dirname + '/public/favicon.ico'));
 // the __dirname is the current directory from where the script is running
-const publicPath = path.join(__dirname, '..', 'public');
-app.use(express.static(publicPath));
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/ping', function (req, res) {
  return res.send('pong');
 });
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(publicPath, 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-app.listen(port, () => { console.log(`Server is up on port ${port}!`); });
-
+app.listen(port);
